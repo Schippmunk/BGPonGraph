71eecc4a98231ba6dc5d19326d65bc44858671eb
Schippmunk <johannes.struve@gmx.de>
2018-11-12 23:53:56 +0000

    - a few more lines, not compiling

--- creator.py
+++ creator.py
@@ -1,10 +1,14 @@
-
-
+# system vars
 filename = 'bgp.pml'
 file_list = []
 
+# program vars
+n = 256
+# nodes are 0 to 3, 0 is target
+nodes = 4
+edges = ()
 
-def savefile():
+def save_file():
     content = "\n".join(file_list)
     with open(filename, 'w+') as the_file:
         the_file.write(content)
@@ -17,10 +21,20 @@ def app(lines):
     else:
         file_list = file_list + lines
 
+def preprocessor():
+    """Adds preprocessor cmds like define to the beginning of the list"""
+    app('#define n ' + str(n))
+
+
+def create_edges():
+    global edges
+    for i in range(nodes):
+        edges.append([i+1, i])
+
 
 def main():
-    app('abc')
-    app(['d', 'e' ,'f'])
+    create_edges()
+    preprocessor()
     savefile()
 
 if __name__ == "__main__":
