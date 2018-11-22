f8da83aca280b64cbb9eb89fdd411ae3309d72ad
Schippmunk <johannes.struve@gmx.de>
2018-11-22 18:43:00 +0000

    - added like two lines to creator.py

--- creator.py
+++ creator.py
@@ -8,23 +8,29 @@ n = 256
 nodes = 4
 edges = ()
 
-def save_file():
+def save_file() -> None:
+    """Saves all the lines of file_list to the file filename"""
     content = "\n".join(file_list)
     with open(filename, 'w+') as the_file:
         the_file.write(content)
 
 
-def app(lines):
+def app(lines) -> None:
+    """lines is a string or an array of strings. This function appends it to the file_list"""
     global file_list
     if isinstance(lines, str):
         file_list.append(lines)
     else:
         file_list = file_list + lines
 
-def preprocessor():
+def preprocessor() -> None:
     """Adds preprocessor cmds like define to the beginning of the list"""
+
     app('#define n ' + str(n))
 
+def create_channels() -> None:
+    for i in range(0..nodes):
+        app("chan t" + str(i) + " = [1] of {byte}")
 
 def create_edges():
     global edges
